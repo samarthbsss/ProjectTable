326e2476d00873b6d94fd138276ce29f6fbe5adf
Samarth <samarthbsacharya@gmail.com>
2023-09-02 16:11:16 +0530

    addded table content

--- src/Components/data.jsx
+++ src/Components/data.jsx
@@ -20,7 +20,7 @@ export const Data = (props) => {
 
   const fetchData = async () => {
     try {
-      const res = await axios.get(`api/project/${item}`);
+      const res = await axios.get(`api/project/&{item}`);
       setprojectdata(res.data);
       console.log("fetch successful");
       console.log(projectdata);
@@ -36,13 +36,13 @@ export const Data = (props) => {
       <div id="content">
         <table id="maintable">
           <tbody>
-            {/* <caption>helo</caption> */}
+         
             <tr>
               <th colSpan="6">
                 <h1>Document Required for Quality Dossier From Ndt</h1>
               </th>
             </tr>
-            {/* </tr> */}
+        
             <tr>
               <td>
                 <h1>Customer :</h1>
@@ -78,7 +78,6 @@ export const Data = (props) => {
             <tr>
               <th scope="row">Bottom</th>
               <td></td>
-              {/* <td rowSpan={2}></td> */}
               <td></td>
             </tr>
             <tr>
@@ -97,7 +96,6 @@ export const Data = (props) => {
             <tr>
               <th scope="row">Bottom</th>
               <td></td>
-              {/* <td rowSpan={2}></td> */}
               <td></td>
             </tr>
 
@@ -117,13 +115,13 @@ export const Data = (props) => {
             <tr>
               <th scope="row">Bottom</th>
               <td></td>
-              {/* <td rowSpan={2}></td> */}
+             
               <td></td>
             </tr>
             <tr>
               <th scope="rowgroup">4</th>
               <th colSpan={2} scope="rowgroup">
-                Stop $ Emergency valve Body -I
+                Stop & Emergency valve Body -I
               </th>
 
               <td></td>
@@ -133,10 +131,8 @@ export const Data = (props) => {
             <tr>
               <th scope="rowgroup">5</th>
               <th colSpan={2} scope="rowgroup">
-                Stop $ Emergency valve Body -II
+                Stop & Emergency valve Body -II
               </th>
-
-              {/* <th scope="row">Top</th> */}
               <td></td>
               <td></td>
               <td></td>
@@ -146,10 +142,8 @@ export const Data = (props) => {
                 6
               </th>
               <th  colSpan={2}scope="rowgroup">
-                Stop $ Emergency valve Column -I
+                Stop & Emergency valve Column -I
               </th>
-
-              {/* <th scope="row">Top</th> */}
               <td></td>
               <td ></td>
               <td></td>
@@ -159,10 +153,8 @@ export const Data = (props) => {
                 6
               </th>
               <th  colSpan={2}scope="rowgroup">
-                Stop $ Emergency valve Column -II
+                Stop & Emergency valve Column -II
               </th>
-
-              {/* <th scope="row">Top</th> */}
               <td></td>
               <td ></td>
               <td></td>
@@ -174,8 +166,6 @@ export const Data = (props) => {
               <th  colSpan={2}scope="rowgroup">
                Nozzle Chest
               </th>
-
-              {/* <th scope="row">Top</th> */}
               <td></td>
               <td ></td>
               <td></td>
@@ -187,18 +177,16 @@ export const Data = (props) => {
               <th  colSpan={2}scope="rowgroup">
              Throttle Valve Column -I
               </th>
-
-              {/* <th scope="row">Top</th> */}
               <td></td>
               <td ></td>
               <td></td>
             </tr>
             <tr>
               <th  scope="rowgroup">
-              9
+              10
               </th>
               <th  colSpan={2}scope="rowgroup">
-             Throttle Valve Column -I
+             Throttle Valve Column -II
               </th>
 
               {/* <th scope="row">Top</th> */}
@@ -226,12 +214,88 @@ export const Data = (props) => {
               <th  colSpan={2}scope="rowgroup">
              Throttle Valve Column -IV
               </th>
-
-              {/* <th scope="row">Top</th> */}
               <td></td>
               <td ></td>
               <td></td>
             </tr>
+            <tr>
+              <th  scope="rowgroup">
+              13
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+            Pass Out Manifold
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+              14
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+             Pass in valve chest
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+              15
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+             Pass in valve Column -I
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+              16
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+             Pass in valve Column -II
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+              17
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+             Pass in valve Column -III
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+              18
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+             Pass in valve Column -IV
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+              19
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+             Throttle Valve Box
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+
             <tr>
               <th rowSpan={2} scope="rowgroup">
                 20
@@ -241,17 +305,125 @@ export const Data = (props) => {
               </th>
 
               <th scope="row">Hp</th>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td rowSpan={2}></td>
+              <td rowSpan={2}></td>
+              <td rowSpan={2}></td>
             </tr>
             <tr>
               <th scope="row">Grid value /LP</th>
+             
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             21
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+             Rotor
+              </th>
               <td></td>
+              <td colSpan={2}></td>
+              {/* <td></td> */}
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             22
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+             Nozzle
+              </th>
               <td></td>
+              <td ></td>
               <td></td>
             </tr>
-
+            <tr>
+              <th  scope="rowgroup">
+             23
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+            Blades
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             23
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+            Blades
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             24
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+            Gear Box Bottom
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             25
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+            Control Valve Body
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             26
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+           Baseplate
+              </th>
+              <td colSpan={2}></td>
+              {/* <td ></td> */}
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             27
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+           Gearbox
+              </th>
+              <td colSpan={2}></td>
+              {/* <td ></td> */}
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             28
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+            Hp Pedestal
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th  scope="rowgroup">
+             29
+              </th>
+              <th  colSpan={2}scope="rowgroup">
+           Lp Pedestal
+              </th>
+              <td></td>
+              <td ></td>
+              <td></td>
+            </tr>
+{/* here */}
             <tr>
               <th rowSpan={2} scope="rowgroup">
                 30
@@ -262,14 +434,13 @@ export const Data = (props) => {
 
               <th scope="row">Top</th>
               <td></td>
-              <td></td>
-              <td></td>
+              <td colSpan={6} rowSpan={8}></td>
+              {/* <td colSpan={9}></td> */}
             </tr>
             <tr>
               <th scope="row">Bottom</th>
               <td></td>
-              <td></td>
-              <td></td>
+              {/* <td colSpan={6} rowSpan={7}></td> */}
             </tr>
             <tr>
               <th rowSpan={2} scope="rowgroup">
@@ -281,13 +452,13 @@ export const Data = (props) => {
 
               <th scope="row">Top</th>
               <td></td>
-              <td></td>
-              <td colSpan={8}></td>
+              
+             
             </tr>
             <tr>
               <th scope="row">Bottom</th>
               <td></td>
-              <td colSpan={4} rowSpan={5}></td>
+             
             </tr>
             <tr>
               <th rowSpan={2} scope="rowgroup">
@@ -299,13 +470,12 @@ export const Data = (props) => {
 
               <th scope="row">Top</th>
               <td></td>
-              {/* <td ></td>
-              <td></td> */}
+              
             </tr>
             <tr>
               <th scope="row">Bottom</th>
               <td></td>
-              {/* <td ></td> */}
+             
             </tr>
             <tr>
               <th rowSpan={2} scope="rowgroup">
@@ -317,13 +487,12 @@ export const Data = (props) => {
 
               <th scope="row">Top</th>
               <td></td>
-              {/* <td ></td>
-              <td></td> */}
+             
             </tr>
             <tr>
               <th scope="row">Bottom</th>
               <td></td>
-              {/* <td ></td> */}
+             
             </tr>
           </tbody>
         </table>
